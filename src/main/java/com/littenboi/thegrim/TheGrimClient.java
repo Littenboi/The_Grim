4ad112933b1dc6534c2ad6a373888319b159372e
Littenboi <littenboi247@gmail.com>
2025-06-08 11:46:42 -0500

    Initial commit

---
+++ src/main/java/com/littenboi/thegrim/TheGrimClient.java
@@ -0,0 +1,10 @@
+package com.littenboi.thegrim;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheGrimClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/littenboi/thegrim/TheGrimDataGenerator.java
@@ -0,0 +1,11 @@
+package com.littenboi.thegrim;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TheGrimDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
